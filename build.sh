9dbe623af916847f3f3571718ea4c5f615b0e2c2
dimmuramos04 <sebastian.ramos5@gmail.com>
2025-08-13 00:22:27 -0400

    Corregir build.sh y gitignore

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 echo "Instalando dependencias..."
 pip install -r requirements.txt
 
-echo "Aplicando migraciones de la base de datos..."
+# Forzar un reinicio completo de la base de datos para limpiar el estado anterior
+echo "Limpiando y reiniciando la base de datos..."
+flask db downgrade base
+
+echo "Aplicando todas las migraciones desde cero..."
 flask db upgrade
 
 echo "Poblando la base de datos con datos iniciales..."
