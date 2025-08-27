7045f5b7fa3e75def7b6c2bc902f574f505dd471
dimmuramos04 <sebastian.ramos5@gmail.com>
2025-08-27 10:45:16 -0400

    fix(app): Aplica monkey-patch de gevent para prevenir errores de bloqueo
    Se agrega gevent.monkey.patch_all() al inicio de app.py para solucionar el RuntimeError causado por llamadas síncronas de la base de datos en un entorno asíncrono de Flask-SocketIO, lo que generaba un error en Sentry.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
